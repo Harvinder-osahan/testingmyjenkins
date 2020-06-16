dbade8a55ea6d4c461a06bee4caf52c0ca00e4fe
Harvinder Singh <46685955+Harvinder-osahan@users.noreply.github.com>
2020-06-16 17:28:11 +0530

    Update task.groovy

--- task.groovy
+++ task.groovy
@@ -31,8 +31,6 @@ job("LaunchingDockerContainer"){
 	         }
                 }
 	steps{
-         shell('if sudo kubectl get deployments | grep mygamedep; then  exit 0; 
-	         else sudo cd /root/DevOpsAL;  sudo kubectl create -f /root/DevOpsAL/deploy.yml;
-                  sudo kubectl expose deployment mygamedep --port=80 --type=NodePort;fi')
+         shell('if sudo kubectl get deployments | grep mygamedep; then  exit 0; else sudo cd /root/DevOpsAL;  sudo kubectl create -f /root/DevOpsAL/deploy.yml;  sudo kubectl expose deployment mygamedep --port=80 --type=NodePort;fi')
         }   
 }
