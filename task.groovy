e67a5b9493e2d016d502b6dc444915a176508000
Harvinder Singh <46685955+Harvinder-osahan@users.noreply.github.com>
2020-06-16 17:17:39 +0530

    Update task.groovy

--- task.groovy
+++ task.groovy
@@ -1,13 +1,34 @@
 job("JobFromGroovy"){
+
+	steps{
+         shell('sleep 60')
+        }       
+}
+
+job("LaunchingDockerContainer"){
 	description("Launching a Docker Container")
 	
         scm {
-              github('Harvinder-osahan/testingmyjenkins', 'master')
+              github('Harvinder-osahan/testingmyjenkins', 'k8s2')
              }
         triggers{
                 scm("* * * * *")
                 }
+         
 	steps{
-         shell('sudo docker run -dit --name webgame -p 1919:80 -v  "$PWD":/usr/local/apache2/htdocs/   httpd')
-        }       
+         shell('sudo docker run -dit --name webgame -p 1919:80 "$PWD":/usr/local/apache2/htdocs/  httpd')
+        }   
+ 	
+
+}
+
+job("LaunchingDockerContainer"){
+	description("Launching a deployment on Kubernetes ")
+	       
+	steps{
+         shell('if sudo kubectl get all | grep mygamedep; then;  exit 0; else; 
+                 sudo cd /root/DevOpsAL;  sudo kubectl create -f /root/DevOpsAL/deploy.yml;
+                  sudo kubectl expose deployment mygamedep --port=80 --type=NodePort;fi')
+        }   
+ 
 }
